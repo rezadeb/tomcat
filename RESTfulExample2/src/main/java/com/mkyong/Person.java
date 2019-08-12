e6483a31ea8686e434349389f4fd65a46dec7f07
rezadeb <mr.maulana8@gmail.com>
2019-08-12 16:27:59 +0700

    init
    init upload

---
+++ RESTfulExample2/src/main/java/com/mkyong/Person.java
@@ -0,0 +1,16 @@
+package com.mkyong;
+
+public class Person {
+
+    String _id;
+
+    public String getId() {
+        return _id;
+    }
+
+    public void setId(String _id) {
+        this._id = _id;
+    }
+        
+       
+}
---
+++ RESTfulExample2/src/main/java/com/mkyong/rest/JSONService.java
@@ -0,0 +1,83 @@
+package com.mkyong.rest;
+
+import com.mkyong.Person;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.mkyong.Track;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("/json/metallica")
+public class JSONService {
+
+	@GET
+	@Path("/get")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Track getTrackInJSON() {
+
+		Track track = new Track();
+		track.setTitle("Enter Sandman");
+		track.setSinger("Metallica");
+
+		return track;
+
+	}
+        
+        @GET
+	@Path("/get/people")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getPeople() {
+
+                
+                String output = "";
+            
+		try {
+
+                    
+			Client client = Client.create();
+
+			WebResource webResource = client
+					.resource("http://localhost:7000/people");
+
+			ClientResponse response = webResource.accept("application/json")
+					.get(ClientResponse.class);
+
+			if (response.getStatus() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : "
+						+ response.getStatus());
+			}
+
+			 output = response.getEntity(String.class);
+
+			System.out.println("Output from Server .... \n");
+			System.out.println(output);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+                return output;
+
+	}
+
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createTrackInJSON(Track track) {
+
+		String result = "Track saved : " + track;
+		return Response.status(201).entity(result).build();
+		
+	}
+	
+}
